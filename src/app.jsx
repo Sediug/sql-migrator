279136c0be39cdd1e977c0e023f57bb5744bb75d
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-03-17 22:21:23 +0100

    Initial commit. Create Electron app.

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React!</h2>
+      </div>
+    );
+  }
+}
